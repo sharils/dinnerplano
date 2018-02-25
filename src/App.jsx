b938577100b1e1e29b21435d68f58f28039b1c1c
sharils <sharils@users.noreply.github.com>
2018-02-25 17:56:46 +0800

    Add redux-saga

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
 import logo from "./logo.svg";
 import "./App.css";
-import reducers from "./reducers";
+import StoreProvider from "./StoreProvider";
 import LoginForm from "./login/LoginForm";
-
-const store = createStore(
-  reducers,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 function App() {
   return (
-    <Provider store={store}>
+    <StoreProvider>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -25,7 +17,7 @@ function App() {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    </Provider>
+    </StoreProvider>
   );
 }
 
---
+++ src/StoreProvider.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+import createSagaMiddleware from "redux-saga";
+import "./App.css";
+import reducers from "./reducers";
+import sagas from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(sagas);
+
+export default function StoreProvider({ children }) {
+  return <Provider store={store}>{children}</Provider>;
+}
+StoreProvider.propTypes = {
+  children: PropTypes.element.isRequired
+};
