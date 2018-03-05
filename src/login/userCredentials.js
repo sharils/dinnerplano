bef4fea464051aeccb024a50f183aea9edf7cef1
sharils <sharils@users.noreply.github.com>
2018-03-05 23:22:25 +0800

    Guard login saga against logged in scenario

--- src/login/userCredentials.js
+++ src/login/userCredentials.js
@@ -1,6 +1,6 @@
 import { createAction, handleAction } from "redux-actions";
 import { SubmissionError } from "redux-form";
-import { call, put, takeLatest } from "redux-saga/effects";
+import { call, put, select, takeLatest } from "redux-saga/effects";
 import { signInAndRetrieveDataWithEmailAndPassword } from "../init/firebaseApp";
 import { fbIsLoginError } from "../util/firebase";
 import { rfCreateAction } from "../util/reduxForm";
@@ -16,6 +16,13 @@ const endLogin = createAction(END);
 export default handleAction(endLogin, (state, { payload }) => payload, null);
 
 function* login({ payload: { email, password }, meta: [resolve, reject] }) {
+  if (yield select(isLoggedIn)) {
+    const e = new Error("User is already logged in");
+    e.name = "DevelopmentTimeError";
+    resolve();
+    throw e;
+  }
+
   try {
     const userCredentials = yield call(
       signInAndRetrieveDataWithEmailAndPassword,
