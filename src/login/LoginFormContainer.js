421cafed21149f0b105105569378e44f46f0170e
sharils <sharils@users.noreply.github.com>
2018-03-15 22:47:47 +0800

    Change reduxForm api

--- src/login/LoginFormContainer.js
+++ src/login/LoginFormContainer.js
@@ -9,16 +9,14 @@ import {
   withProps
 } from "recompose";
 import { createStructuredSelector } from "reselect";
-import { rfBindActionCreators } from "../util/reduxForm";
+import { rfPromisifyOnSubmits } from "../util/reduxForm";
 import LoginForm from "./LoginForm";
 import { isLoggedIn, startLogin } from "./userCredentials";
 
 const mapStateToProps = createStructuredSelector({
   isLoggedIn
 });
-const mapDispatchToProps = rfBindActionCreators.bind(null, {
-  onSubmit: startLogin
-});
+const mapDispatchToProps = rfPromisifyOnSubmits({ onSubmit: startLogin });
 const redirect = compose(withProps({ to: "/" }), renderComponent(Redirect));
 
 export default compose(
--- src/util/reduxForm.js
+++ src/util/reduxForm.js
@@ -1,14 +1,37 @@
-import { mapObjIndexed } from "ramda";
+import { map } from "ramda";
+import { bindActionCreators } from "redux";
 import { createAction } from "redux-actions";
 
 const onSubmitCreator = dispatch => actionCreator => payload =>
   new Promise((...meta) => dispatch(actionCreator({ payload, meta })));
 
-export const rfBindActionCreators = (actionCreators, dispatch) =>
-  mapObjIndexed(onSubmitCreator(dispatch), actionCreators);
+const rfBindActionCreators = (actionCreators, dispatch) =>
+  map(onSubmitCreator(dispatch), actionCreators);
 
 export const rfCreateAction = actionType =>
   createAction(actionType, ({ payload }) => payload, ({ meta }) => meta);
 
 export const rfGetSubmitDisabled = props =>
   !props.anyTouched || props.pristine || props.submitting;
+
+const onSubmitKeysDefaults = { onSubmit: true };
+
+export const rfPromisifyOnSubmits = (
+  actionCreators,
+  onSubmitKeys = onSubmitKeysDefaults
+) => {
+  const notOnSubmits = Object.keys(actionCreators).filter(
+    key => !onSubmitKeys[key]
+  );
+  const onSubmits = Object.keys(onSubmitKeys).reduce(
+    (accu, onSubmitKey) => ({
+      ...accu,
+      [onSubmitKey]: actionCreators[onSubmitKey]
+    }),
+    {}
+  );
+  return dispatch => ({
+    ...bindActionCreators(notOnSubmits, dispatch),
+    ...rfBindActionCreators(onSubmits, dispatch)
+  });
+};
